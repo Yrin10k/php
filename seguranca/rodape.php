385334736a3e8da97588eb510aa4a4ae467f29dd
Ayron Nascimento <128555395+Yrin10k@users.noreply.github.com>
2023-06-29 15:20:20 -0300

    Segurança

---
+++ seguranca/rodape.php
@@ -0,0 +1,24 @@
+ 
+<head>
+    <link rel="stylesheet" href="css/estilo.css">
+  
+</head>
+ 
+ 
+<footer>
+    <div id="rodape">
+    <img src="images/logobairro.jpg" class="imgrodape">
+    <p class="txtrodape">R. Cônego Aníbal Maria Di Francia, 1483 -<br> Pinheirinho, Criciúma - SC, 88804-360
+        <br>
+        <br>
+        <img src="images/minizap.png" class="zap">(48) 3403-2700
+        <br>
+        <img src="images/email.jpg" class="email"> secretariacep@bairrodajuventude.org.br</p>
+    
+    </div>
+    <p class="bottom">Copyright © 2023 - Desenvolvido pelo curso técnico de programação</p>
+    <br>
+    <br>
+    <br>
+</footer>
+ 
